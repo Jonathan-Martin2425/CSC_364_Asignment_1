ad342d940efcd9de67c8fd90b61913665aa91ba3
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2026-02-12 17:11:11 -0800

    reverse y direction

--- src/main/java/WindowView.java
+++ src/main/java/WindowView.java
@@ -31,11 +31,11 @@ public class WindowView extends JPanel implements PropertyChangeListener {
                 Point player = pub.getPlayer();
                 Point newPos = player;
                 if(e.getKeyChar() == 'w'){
-                    newPos = new Point(player.x, player.y + 1);
+                    newPos = new Point(player.x, player.y - 1);
                 }else if(e.getKeyChar() == 'a'){
                     newPos = new Point(player.x - 1, player.y);
                 }else if(e.getKeyChar() == 's'){
-                    newPos = new Point(player.x, player.y - 1);
+                    newPos = new Point(player.x, player.y + 1);
                 }else if(e.getKeyChar() == 'd'){
                     newPos = new Point(player.x + 1, player.y);
                 }
